d15ea8710db05e507ea95a8f264673143d54bbd9
Unknown <thatoneguy1c@gmail.com>
2018-04-09 11:26:00 -0400

    Second level 75%
    BRAND NEW PROJECT FOLDER

---
+++ GoodGoodHandy/Assets/LevelEndScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelEndScript : MonoBehaviour {
+
+    public float fastFallSpeed;
+    public float fallSpeed;
+    public float enteranceYPos = 11;
+    public float missYPos = -8;
+    public string sceneName;
+
+    void Start () {
+       
+    }
+	
+
+	void Update () {
+        transform.position += new Vector3(0, (-fallSpeed * Time.deltaTime), 0);
+        if(transform.position.y < enteranceYPos)
+        {
+            fallSpeed = fastFallSpeed;
+
+            if (transform.position.y < missYPos)
+            {
+                SceneManager.LoadScene(sceneName);
+            }
+        }
+        
+    }
+}
